6a56c141e3638b6a4e0a23f1cef7fa15ccbbef3c
Muhammad Nabeel <nabeelrafique101@gmail.com>
2026-03-02 22:20:33 +0500

    Delete Quiz 1 directory

--- Quiz 1/Dice App/android/app/src/main/kotlin/com/example/dice3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dice3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Quiz 1/Dice App/assets/sounds/README.txt
+++
@@ -1,2 +0,0 @@
-Only custom sound in use:
-- ludo_roll.mp3 (from Downloads/Dice Roll 5.mp3)
