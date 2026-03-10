2a2770869d8511c91e6c87d066392a6960a39cd2
Muhammad Nabeel <nabeelrafique101@gmail.com>
2026-03-10 21:19:39 +0500

    Delete Lab Assignment 1 directory

--- Lab Assignment 1/lib/utils/image_provider.dart
+++
@@ -1,9 +0,0 @@
-﻿import 'package:flutter/material.dart';
-
-import 'image_provider_stub.dart'
-    if (dart.library.io) 'image_provider_io.dart'
-    if (dart.library.html) 'image_provider_web.dart';
-
-ImageProvider resolveImageProvider(String uri) {
-  return buildImageProvider(uri);
-}
--- Lab Assignment 1/lib/utils/image_provider_stub.dart
+++
@@ -1,5 +0,0 @@
-﻿import 'package:flutter/material.dart';
-
-ImageProvider buildImageProvider(String uri) {
-  return const AssetImage('');
-}
